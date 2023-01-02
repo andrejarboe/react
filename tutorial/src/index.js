5a1eb8b06e4c2abd386d1c117291553f8b34cf2c
Andre Jarboe <andrejarboe@gmail.com>
2023-01-02 16:58:03 -0500

    refactor

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -21,34 +21,29 @@ function Greating() {
 }
 
 const BookList = () => {
-    return <section className='booklist'>
+  return (
+    <section className="booklist">
       <Book />
       <Book />
       <Book />
       <Book />
-  </section>
+    </section>
+  )
 }
 
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL600_SR600,400_.jpg"
+        alt="Atomic Habits"
+      />
+      <h2>Atomic Habits</h2>
+      <h4>James Clear</h4>
     </article>
   )
 }
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL600_SR600,400_.jpg"
-    alt="Atomic Habits"
-  />
-)
-const Title = () => {
-  return <h2>Atomic Habits</h2>
-}
-const Author = () => <h4>James Clear</h4>
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 
