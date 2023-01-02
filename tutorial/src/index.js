6c12192028afdba5ffdeff35b7efd8c681536dd1
Andre Jarboe <andrejarboe@gmail.com>
2023-01-02 16:39:28 -0500

    books

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -7,14 +7,45 @@ function Greating() {
       <h2>
         <a
           target="_blank"
+          rel="noreferrer"
           href="https://github.com/john-smilga/react-course-v3/tree/main/01-fundamentals"
         >
           React Course Notes!
         </a>
       </h2>
+      <BookList />
+      <BookList />
+      <BookList />
     </div>
   )
 }
+
+const BookList = () => {
+    return <section>
+      <Book />
+  </section>
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL600_SR600,400_.jpg"
+    alt="Atomic Habits"
+  />
+)
+const Title = () => {
+  return <h2>Atomic Habits</h2>
+}
+const Author = () => <h4>James Clear</h4>
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 
