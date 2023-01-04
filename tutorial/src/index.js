9a30d557f6decd65f2f790f422dd4cf4d760c052
Andre Jarboe <andrejarboe@gmail.com>
2023-01-04 15:37:00 -0500

    add submit

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -84,28 +84,40 @@ const BookList = () => {
 };
 
 const EventExamples = () => {
-    const handleFormInput = () => {
-      console.log('handle form input');
-    };
-    const handleButtonClick = () => {
-      alert('handle button click');
-    };
+  const handleFormInput = (e) => {
+    console.log(e);
+    // e.target - element
+    console.log(`Input Name : ${e.target.name}`);
+    console.log(`Input Value : ${e.target.value}`);
+    // console.log('handle form input');
+  };
 
-    return (
-      <section>
-        <form>
-          <h2>Typical Form</h2>
-          <input
-            type="text"
-            name="example"
-            onChange={handleFormInput}
-            style={{ margin: '1rem 0' }}
-          />
-        </form>
-        <button onClick={handleButtonClick}>click me</button>
-      </section>
-    );
-}
+  const handleButtonClick = () => {
+    alert('handle button click');
+  };
+
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+  };
+  return (
+    <section>
+      {/* add onSubmit Event Handler */}
+      <form onSubmit={handleFormSubmission}>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+        {/* add button with type='submit' */}
+        <button type="submit">submit form</button>
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
+    </section>
+  );
+};
 
 const Book = (props) => {
   const { imgSrc, author, title, children } = props;
